6e344ca38953481cfb065f7f87fbe8658b076c4c
Zach Stone <zachinjapan888@gmail.com>
2021-12-02 15:35:25 -0500

    remove confetti

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,8 @@
 import "../Footer/Footer.css";
 import SocialLinks from "../UI/SocialLinks";
 import { GiPartyPopper } from "react-icons/gi";
-import Confetti from "react-confetti";
-import { useState } from "react";
-import useWindowSize from "react-use/lib/useWindowSize";
 
 const Footer = (props) => {
-  const { width, height } = useWindowSize();
-
-  const [confetti, setConfetti] = useState(true);
-
   let content = {
     English: {
       description: "Zach Stone 2021 .... ",
@@ -23,40 +16,20 @@ const Footer = (props) => {
     ? (content = content.Japanese)
     : (content = content.English);
 
-  if (confetti === true) {
-    return (
-      <>
-        <Confetti width={width} height={height} numberOfPieces={5000} />;
-        <footer>
-          <div class="footer-text">
-            <span>
-              {content.description}{" "}
-              <button>
-                {" "}
-                <GiPartyPopper />
-              </button>
-            </span>
-          </div>
-          <SocialLinks />
-        </footer>
-      </>
-    );
-  } else {
-    return (
-      <footer>
-        <div class="footer-text">
-          <span>
-            {content.description}{" "}
-            <button>
-              {" "}
-              <GiPartyPopper />
-            </button>
-          </span>
-        </div>
-        <SocialLinks />
-      </footer>
-    );
-  }
+  return (
+    <footer>
+      <div class="footer-text">
+        <span>
+          {content.description}{" "}
+          <button>
+            {" "}
+            <GiPartyPopper />
+          </button>
+        </span>
+      </div>
+      <SocialLinks />
+    </footer>
+  );
 };
 
 export default Footer;
