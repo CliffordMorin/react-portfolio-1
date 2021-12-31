961e6a93f66e727385b6fd6fff2fd0a046b7a2af
Zach Stone <zachinjapan888@gmail.com>
2021-12-31 16:29:05 -0500

    bio change

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,7 @@ const About = (props) => {
     English: {
       title: "About Me",
       description:
-        "Hi, I'm Zach.  I'm a self-taught web developer who can speak Japanese. I have experience building pages using vanilla Javascript as well as React frameworks, such as create-react-app or Gatsby.js. ",
+        "Hi, I'm Zach.  I'm a self-taught web developer who can speak Japanese.",
       description2:
         "I am currently learning authentication and database management and will study at a coding bootcamp in Japan in 2022.",
       description3:
