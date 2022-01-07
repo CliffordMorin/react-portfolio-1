c68d213bea3daa93a0ebf960e794d576431660d9
Zach Stone <zachinjapan888@gmail.com>
2022-01-07 18:19:47 -0500

    more projects button

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,13 +8,17 @@ import castles from "../../images/work/castles.webp";
 import MUIProjectCard from "./ProjectCard/MUIProjectCard";
 import Slide from "react-reveal/Slide";
 import Fade from "react-reveal/Fade";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 
 const Projects = (props) => {
+  const [showMoreProjects, setShowMoreProjects] = useState(false);
+
   let content = {
     English: {
       mainTitle: "Projects",
       subTitle: "Side Projects",
-      subTitleButton: "See more of my work",
+      moreProjectsButton: "...More Projects",
 
       projects: [
         {
@@ -105,7 +109,7 @@ const Projects = (props) => {
     Japanese: {
       mainTitle: "プロジェクト",
       subTitle: "サイドプロジェクト",
-      subTitleButton: "サイドプロジェクトを見る",
+      moreProjectsButton: "もっとプロジェクトを見る",
       projects: [
         {
           title: "引用クイズ",
@@ -243,50 +247,64 @@ const Projects = (props) => {
             />
           </div>
         </Slide>
-        <Slide left>
-          <>
-            {/* <h1>{content.subTitle}</h1> */}
-            <div className="projects-container">
-              <MUIProjectCard
-                title={content.projects[1].title}
-                description={content.projects[1].description}
-                image={content.projects[1].image}
-                link={content.projects[1].link}
-                linkText={content.projects[1].linkText}
-                github={content.projects[1].github}
-                tags={content.projects[1].tags}
-                tech={content.projects[1].tech}
-                tech_description={content.projects[1].tech_description}
-                callToAction={content.projects[1].callToAction}
-              />
-              <MUIProjectCard
-                title={content.projects[2].title}
-                description={content.projects[2].description}
-                image={content.projects[2].image}
-                link={content.projects[2].link}
-                linkText={content.projects[2].linkText}
-                github={content.projects[2].github}
-                tags={content.projects[2].tags}
-                tech={content.projects[2].tech}
-                tech_description={content.projects[2].tech_description}
-                callToAction={content.projects[2].callToAction}
-              />
+        {!showMoreProjects ? (
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => {
+              setShowMoreProjects(true);
+            }}
+          >
+            {content.moreProjectsButton}
+          </Button>
+        ) : null}
 
-              <MUIProjectCard
-                title={content.projects[5].title}
-                description={content.projects[5].description}
-                image={content.projects[5].image}
-                link={content.projects[5].link}
-                linkText={content.projects[5].linkText}
-                github={content.projects[5].github}
-                tags={content.projects[5].tags}
-                tech={content.projects[5].tech}
-                tech_description={content.projects[5].tech_description}
-                callToAction={content.projects[5].callToAction}
-              />
-            </div>
-          </>
-        </Slide>
+        {showMoreProjects ? (
+          <Slide left>
+            <>
+              {/* <h1>{content.subTitle}</h1> */}
+              <div className="projects-container">
+                <MUIProjectCard
+                  title={content.projects[1].title}
+                  description={content.projects[1].description}
+                  image={content.projects[1].image}
+                  link={content.projects[1].link}
+                  linkText={content.projects[1].linkText}
+                  github={content.projects[1].github}
+                  tags={content.projects[1].tags}
+                  tech={content.projects[1].tech}
+                  tech_description={content.projects[1].tech_description}
+                  callToAction={content.projects[1].callToAction}
+                />
+                <MUIProjectCard
+                  title={content.projects[2].title}
+                  description={content.projects[2].description}
+                  image={content.projects[2].image}
+                  link={content.projects[2].link}
+                  linkText={content.projects[2].linkText}
+                  github={content.projects[2].github}
+                  tags={content.projects[2].tags}
+                  tech={content.projects[2].tech}
+                  tech_description={content.projects[2].tech_description}
+                  callToAction={content.projects[2].callToAction}
+                />
+
+                <MUIProjectCard
+                  title={content.projects[5].title}
+                  description={content.projects[5].description}
+                  image={content.projects[5].image}
+                  link={content.projects[5].link}
+                  linkText={content.projects[5].linkText}
+                  github={content.projects[5].github}
+                  tags={content.projects[5].tags}
+                  tech={content.projects[5].tech}
+                  tech_description={content.projects[5].tech_description}
+                  callToAction={content.projects[5].callToAction}
+                />
+              </div>
+            </>
+          </Slide>
+        ) : null}
       </div>
     </>
   );
