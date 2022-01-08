043bd8855e72ca7c1df4271c2161ffb42b73ff8d
Zach Stone <zachinjapan888@gmail.com>
2022-01-08 18:36:57 -0500

    birds

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,8 +3,36 @@ import SocialLinksVertical from "../UI/SocialLinksVertical";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { IconContext } from "react-icons";
 import Flip from "react-reveal/Flip";
+import BIRDS from "vanta/dist/vanta.birds.min";
+import React, { useState, useEffect, useRef } from "react";
 
 const Landing = (props) => {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          backgroundColor: 0xdddddd,
+          color1: 0x2e7d32,
+          color2: 0xe62776,
+          colorMode: "lerp",
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   let content = {
     English: {
       title: "Zach Stone",
@@ -24,7 +52,7 @@ const Landing = (props) => {
     <IconContext.Provider
       value={{ color: "#2e7d32", className: "global-class-name", size: "50px" }}
     >
-      <div className="Landing">
+      <div className="Landing" ref={myRef}>
         <div className="landing-content">
           <Flip top>
             <h1 className="landing-title">{content.title}</h1>
