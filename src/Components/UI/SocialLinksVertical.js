56c351cac9d0d538815468ef957ad7596e1992f5
Zach Stone <zachinjapan888@gmail.com>
2022-01-30 08:34:02 -0500

    social links vertical slide and padding

--- src/Components/UI/SocialLinksVertical.js
+++ src/Components/UI/SocialLinksVertical.js
@@ -5,42 +5,49 @@ import { AiOutlineGithub } from "react-icons/ai";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { MdOutlineComputer } from "react-icons/md";
 import { IconContext } from "react-icons";
+import Slide from "react-reveal/Slide";
 
 const SocialLinksVertical = () => {
   return (
     <IconContext.Provider
-      value={{ color: "#2e7d32", className: "global-class-name", size: "40px" }}
+      value={{
+        color: "#2e7d32",
+        className: "global-class-name",
+        size: "50px",
+      }}
     >
-      <div className="social-links-vertical">
-        <a
-          href="https://www.linkedin.com/in/zachinjapan888/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsLinkedin className="social-link-img" />
-        </a>
-        <a
-          href="https://github.com/zachinjapan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AiOutlineGithub className="social-link-img" />
-        </a>
-        <a
-          href="https://twitter.com/zachstonewebdev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AiFillTwitterCircle className="social-link-img" />
-        </a>
-        <a
-          href="https://zachinjapan.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdOutlineComputer className="social-link-img" />
-        </a>
-      </div>
+      <Slide left>
+        <div className="social-links-vertical">
+          <a
+            href="https://www.linkedin.com/in/zachinjapan888/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsLinkedin className="social-link-img" />
+          </a>
+          <a
+            href="https://github.com/zachinjapan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineGithub className="social-link-img" />
+          </a>
+          <a
+            href="https://twitter.com/zachstonewebdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillTwitterCircle className="social-link-img" />
+          </a>
+          <a
+            href="https://zachinjapan.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MdOutlineComputer className="social-link-img" />
+          </a>
+        </div>
+      </Slide>
     </IconContext.Provider>
   );
 };
