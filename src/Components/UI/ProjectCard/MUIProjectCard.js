5b913e888061b031176c6b81b524aa9bbcb4bfe7
Zach Stone <zachinjapan888@gmail.com>
2022-03-10 11:27:57 -0500

    remove unused imports

--- src/Components/UI/ProjectCard/MUIProjectCard.js
+++ src/Components/UI/ProjectCard/MUIProjectCard.js
@@ -5,25 +5,12 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Button from "@mui/material/Button";
 import { AiFillGithub } from "react-icons/ai";
 import { RiComputerLine } from "react-icons/ri";
 import "./ProjectCard.css";
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 export default function MUIProjectCard(props) {
   const TagButton = styled(Button)(({ theme }) => ({
@@ -32,12 +19,6 @@ export default function MUIProjectCard(props) {
     disabled: true,
     ":hover": {},
   }));
-
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     // was 345
@@ -138,11 +119,6 @@ export default function MUIProjectCard(props) {
           </div>
         </div>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{props.tech_description}</Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
