919ef90f72d262033a255e86d94b731de8a5bb97
Zach Stone <zachinjapan888@gmail.com>
2022-07-11 18:23:13 -0400

    add scroll animation to running gif

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -237,7 +237,13 @@ const Projects = (props) => {
     <>
       <div className="Projects" id="Projects">
         <h1>{content.mainTitle}</h1>
-        <RunningImg src={Naruto} />
+        <AnimationOnScroll
+          animateIn="animate__fadeIn"
+          animateOut="animate__fadeOut"
+          animateOnce={true}
+        >
+          <RunningImg src={Naruto} />
+        </AnimationOnScroll>
 
         <div className="projects-container">
           {content.projects.map((project, index) => (
