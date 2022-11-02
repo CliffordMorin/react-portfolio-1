576ff3370af3cecece192c5ba84cb970ba5050ec
Zach Stone <zachinjapan888@gmail.com>
2022-11-02 08:23:00 -0400

    fix spacing issue in Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,28 +5,25 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-text">
-        <span
-          style={{
-            fontSize: "1rem",
-            fontWeight: "bold",
-        
-            
-          }}
-        >
-          Thanks for looking at my portfolio.
-      
-        </span>
-      
-        <span
-          style={{
-            fontSize: "1rem",
-            fontWeight: "bold",
-            color: "var(--secondary-color)",
-            
-          }}
-        >
-          
-          Let's connect!
+        <span>
+          <span
+            style={{
+              fontSize: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            Thanks for looking at my portfolio.
+          </span>
+          <span> </span>
+          <span
+            style={{
+              fontSize: "1rem",
+              fontWeight: "bold",
+              color: "var(--secondary-color)",
+            }}
+          >
+            Let's connect!
+          </span>
         </span>
       </div>
       <SocialLinks />
