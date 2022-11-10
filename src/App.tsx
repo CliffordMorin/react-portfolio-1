2ae7e1e262c80e2094346a72372bf3347c8b1a4e
Zach Stone <zachinjapan888@gmail.com>
2022-11-10 07:41:06 -0500

    lazy load and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import { connect } from "react-redux";
 import "animate.css/animate.min.css";
+import React, { Suspense } from "react";
+import LoadingSpinner from "./Components/UI/LoadingSpinner";
 
-import {
-  LandingV2,  
-  About,
-  Projects,
-  Footer,
-  MUINav,
-  Blog,
-} from "./Components/index";
+import { LandingV2, Footer, MUINav } from "./Components/index";
+const About = React.lazy(() => import("./Components/About/About"));
+const Projects = React.lazy(() => import("./Components/Projects/Projects"));
+const Blog = React.lazy(() => import("./Components/Blog/Blog"));
 
-function App(props : {
-  language: string;
-} ) {
+function App(props: { language: string }) {
   console.log(
     "%cHi! Thanks for checking out my code ☺ \n\nThe repo is on my github as 'React Portfolio'. \n\nFind a bug? Feel free to send me a message using my contact form.\n\n",
     "color:green;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold"
@@ -23,17 +19,17 @@ function App(props : {
     <div className="App">
       <MUINav />
       <LandingV2 language={props.language} />
-      <About language={props.language} />
-      <Projects language={props.language} />
-      <Blog language={props.language} />
-      <Footer  />
+      <Suspense fallback={<LoadingSpinner />}>
+        <About language={props.language} />
+        <Projects language={props.language} />
+        <Blog language={props.language} />
+      </Suspense>
+      <Footer />
     </div>
   );
 }
 
-const mapStateToProps = (state : {
-  language: string;
-}) => {
+const mapStateToProps = (state: { language: string }) => {
   return {
     language: state.language,
   };
---
+++ src/Components/UI/LoadingSpinner.js
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div class="loader-wrapper">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
