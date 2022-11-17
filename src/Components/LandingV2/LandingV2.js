1e05606f7c2f6ee8dda0e98c05b0042aec2512cd
Zach Stone <zachinjapan888@gmail.com>
2022-11-16 21:38:31 -0500

    new landing and color

--- src/Components/LandingV2/LandingV2.js
+++ src/Components/LandingV2/LandingV2.js
@@ -1,67 +1,70 @@
 import "./LandingV2.css";
 import SocialLinksVertical from "../UI/SocialLinksVertical";
-import 'animate.css';
+import "animate.css";
 
 const LandingV2 = (props) => {
-    let content = {
-        English: {
-            morning : "Good Morning",
-            afternoon : "Good Afternoon",
-            evening : "Good Evening",
-          
-        },
-        Japanese: {
-            morning : "おはようございます",
-            afternoon : "こんにちは",
-            evening : "こんばんは",
-          
-        },
-      };
-    
-      props.language === "Japanese"
-        ? (content = content.Japanese)
-        : (content = content.English);
+  let content = {
+    English: {
+      morning: "Good Morning",
+      afternoon: "Good Afternoon",
+      evening: "Good Evening",
+    },
+    Japanese: {
+      morning: "おはようございます",
+      afternoon: "こんにちは",
+      evening: "こんばんは",
+    },
+  };
 
+  props.language === "Japanese"
+    ? (content = content.Japanese)
+    : (content = content.English);
 
-    // find if the user is in the morning, afternoon, or evening
-    const time = new Date().getHours();
-    let currGreeting = "Hello";
-    if (time < 12) {
-        currGreeting = content.morning;
-    } else if (time < 18) {
-        currGreeting =  content.afternoon;
-    } else {
-        currGreeting =  content.evening;
-    }
+  // find if the user is in the morning, afternoon, or evening
+  const time = new Date().getHours();
+  let currGreeting = "Hello";
+  if (time < 12) {
+    currGreeting = content.morning;
+  } else if (time < 18) {
+    currGreeting = content.afternoon;
+  } else {
+    currGreeting = content.evening;
+  }
 
+  return (
+    <div className="landing-v2">
+      <h1 className="landing-v2-title animate__animated animate__fadeIn">
+        <span>{`${currGreeting}`}, I'm </span>
+        <span
+          style={{
+            color: "var(--secondary-color)",
+          }}
+        >
+          Zach
+        </span>
+        <span> and I build </span>
+        <span
+          style={{
+            color: "var(--secondary-color)",
+          }}
+        >
+          {" "}
+          websites
+        </span>
+        <span> and </span>
+        <span
+          style={{
+            color: "var(--third-color)",
+          }}
+        >
+          {" "}
+          iOS applications
+        </span>
+        <span>.</span>
+      </h1>
+      <SocialLinksVertical />
+    </div>
+  );
+};
 
-    return (
-        <div className="landing-v2">
-
-        
-          
-                    <h1 className="landing-v2-title animate__animated animate__fadeIn">
-                        <span>{`${currGreeting}`}, I'm </span>
-                        <span style={{
-
-color : "var(--secondary-color)"
-                        }}>Zach</span>
-                        <span> and I build </span>
-                        <span style ={{
-                            color : "var(--secondary-color)" }
-                        }> web applications</span>
-                        {/* <span> and </span>
-                        <span style ={{
-                            color :"#F5A623" }
-                        }> mobile apps</span> */}
-                        <span>.</span>
-                    </h1>
-                    <SocialLinksVertical />
-      
-        </div>
-    );
-}
-
-
-
-export default LandingV2
+export default LandingV2;
