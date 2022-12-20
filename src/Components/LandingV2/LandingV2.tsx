b3923ec6219e26ad38f07dd90159f17c915c1bf0
Zach Stone <83141990+ZacharyTStone@users.noreply.github.com>
2022-12-20 17:26:01 -0500

    Update LandingV2.tsx

--- src/Components/LandingV2/LandingV2.tsx
+++ src/Components/LandingV2/LandingV2.tsx
@@ -67,11 +67,11 @@ const LandingTile = styled.h1`
 	margin: 50px;
 	font-size: 3rem;
 
-	@media (max-width: 1024px) {
+	@media (768px <= width >= 1024px) {
 		font-size: 2.5rem;
 	}
 
-	@media (max-width: 768px) {
+	@media (width <= 767px) {
 		font-size: 2rem;
 	}
 `;
