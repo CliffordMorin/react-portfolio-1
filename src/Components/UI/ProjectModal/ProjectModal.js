991bd633b93057641ed5efe9189a009d2ea29f3b
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-03-24 15:35:45 -0400

    add limits on modal width for small phones

--- src/Components/UI/ProjectModal/ProjectModal.js
+++ src/Components/UI/ProjectModal/ProjectModal.js
@@ -27,7 +27,8 @@ const customStyles = {
 		backgroundColor: "var(--black)",
 		border: "solid 5px var(--secondary-color)",
 		borderRadius: "10px",
-		maxWidth: "100vw",
+		width: "100%",
+		maxWidth: "99vw",
 	},
 };
 
