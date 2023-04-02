b6aeb1621ef4a2d5f196a973a4164a8f6e3a4be0
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-04-02 12:07:12 -0400

    new recommended blog post

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -21,6 +21,12 @@ const Blog = () => {
 			mainTitle: "Featured Blog Posts",
 			posts: [
 				{
+					title:
+						"Understanding Array References and Creating Copies of Objects in JavaScript",
+					image: "https://media.giphy.com/media/ZxzzPzlimXj9k1M262/giphy.gif",
+					link: "https://zachinjapan.com/Understanding%20Array%20References%20and%20Creating%20Copies%20in%20JavaScript/",
+				},
+				{
 					title: "Three Simple Tricks I Use To Make My Sites Load Faster",
 					image: "https://media.giphy.com/media/vLUhsxMfdVfS8/giphy.gif",
 					link: "https://zachinjapan.com/Three%20Simple%20Tricks%20I%20Use%20To%20Make%20My%20Sites%20Load%20Faster/",
@@ -34,11 +40,6 @@ const Blog = () => {
 					title: "Using Recursion to Insert Values Into a Sorted Binary Tree",
 					image: "https://media.giphy.com/media/qcy6cSzrtP7ybXvZvn/giphy.gif",
 					link: "https://zachinjapan.com/Using%20Recursion%20to%20Insert%20Values%20Into%20a%20Sorted%20Binary%20Tree/",
-				},
-				{
-					title: "An Intro to Recursion",
-					image: "https://media.giphy.com/media/xThuWu82QD3pj4wvEQ/giphy.gif",
-					link: "https://zachinjapan.com/recursion/",
 				},
 				{
 					title: "this.concept = important",
@@ -56,6 +57,12 @@ const Blog = () => {
 			mainTitle: "ブログ記事",
 			posts: [
 				{
+					title:
+						"JavaScriptにおける配列の参照の理解とオブジェクトのコピーの作成 (日本語)",
+					image: "https://media.giphy.com/media/ZxzzPzlimXj9k1M262/giphy.gif",
+					link: "https://zachinjapan.com/Understanding%20Array%20References%20and%20Creating%20Copies%20in%20JavaScript/",
+				},
+				{
 					title: "サイトの読み込みを高速化するため3つの簡単なコツ（英語）",
 					image: "https://media.giphy.com/media/vLUhsxMfdVfS8/giphy.gif",
 					link: "https://zachinjapan.com/Three%20Simple%20Tricks%20I%20Use%20To%20Make%20My%20Sites%20Load%20Faster/",
@@ -69,11 +76,6 @@ const Blog = () => {
 					title: "再帰を使用して、Sorted Binary Treeに番号を挿入方法 （英語）",
 					image: "https://media.giphy.com/media/qcy6cSzrtP7ybXvZvn/giphy.gif",
 					link: "https://zachinjapan.com/Using%20Recursion%20to%20Insert%20Values%20Into%20a%20Sorted%20Binary%20Tree/",
-				},
-				{
-					title: "再帰 (英語）",
-					image: "https://media.giphy.com/media/xThuWu82QD3pj4wvEQ/giphy.gif",
-					link: "https://zachinjapan.com/recursion/",
 				},
 				{
 					title: "this.concept = important （英語）",
