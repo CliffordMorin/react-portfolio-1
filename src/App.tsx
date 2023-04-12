9f29f8cfb773ef79236766d5c6f11470e5388f8e
Clifford Morin <cemorin21@gmail.com>
2023-04-11 20:29:28 -0700

    made background only not clickable on mobile but still there

--- src/App.tsx
+++ src/App.tsx
@@ -66,85 +66,85 @@ function App() {
 				<Overlay />
 			) : (
 				<>
-					{isDesktopOrLaptop && (
-						<Particles
-							id="tsparticles"
-							init={particlesInit}
-							loaded={particlesLoaded}
-							options={{
-								background: {
-									color: {
-										value: "var(--black)",
+					{/* {isDesktopOrLaptop && ( */}
+					<Particles
+						id="tsparticles"
+						init={particlesInit}
+						loaded={particlesLoaded}
+						options={{
+							background: {
+								color: {
+									value: "var(--black)",
+								},
+							},
+							fpsLimit: 60,
+							interactivity: {
+								events: {
+									onClick: {
+										enable: isDesktopOrLaptop,
+										mode: "push",
+									},
+									onHover: {
+										enable: false,
+										mode: "repulse",
+									},
+									resize: true,
+								},
+								modes: {
+									push: {
+										quantity: 4,
+									},
+									repulse: {
+										distance: 200,
+										duration: 0.4,
 									},
 								},
-								fpsLimit: 60,
-								interactivity: {
-									events: {
-										onClick: {
-											enable: true,
-											mode: "push",
-										},
-										onHover: {
-											enable: false,
-											mode: "repulse",
-										},
-										resize: true,
-									},
-									modes: {
-										push: {
-											quantity: 4,
-										},
-										repulse: {
-											distance: 200,
-											duration: 0.4,
-										},
-									},
+							},
+							particles: {
+								color: {
+									value: "#ffffff",
 								},
-								particles: {
-									color: {
-										value: "#ffffff",
-									},
-									links: {
-										color: "#43c396",
-										distance: 150,
-										enable: true,
-										opacity: 0.2,
-										width: 1,
-									},
-									collisions: {
-										enable: true,
-									},
-									move: {
-										direction: "none",
-										enable: true,
-										outModes: {
-											default: "bounce",
-										},
-										random: false,
-										speed: 1,
-										straight: false,
-									},
-									number: {
-										density: {
-											enable: true,
-											area: 800,
-										},
-										value: 40,
-									},
-									opacity: {
-										value: 0.1,
-									},
-									shape: {
-										type: "circle",
-									},
-									size: {
-										value: { min: 1, max: 5 },
-									},
+								links: {
+									color: "#43c396",
+									distance: 150,
+									enable: true,
+									opacity: 0.2,
+									width: 1,
 								},
-								detectRetina: true,
-							}}
-						/>
-					)}
+								collisions: {
+									enable: true,
+								},
+								move: {
+									direction: "none",
+									enable: true,
+									outModes: {
+										default: "bounce",
+									},
+									random: false,
+									speed: 1,
+									straight: false,
+								},
+								number: {
+									density: {
+										enable: true,
+										area: 800,
+									},
+									value: 40,
+								},
+								opacity: {
+									value: 0.1,
+								},
+								shape: {
+									type: "circle",
+								},
+								size: {
+									value: { min: 1, max: 5 },
+								},
+							},
+							detectRetina: true,
+						}}
+					/>
+					{/* )} */}
 
 					<Suspense fallback={<h1>Loading...</h1>}>
 						<LandingV2 />
